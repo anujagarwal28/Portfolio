10529384951faf441965479c89c1c506b870e7de
Anuj Agarwal <anujkrishna2002@gmail.com>
2024-09-20 23:39:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+const App =()=>{
+  return (
+    <Header/>
+  )
+}
+export default App;
