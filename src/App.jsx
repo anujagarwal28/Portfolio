a731716f82068d8e9e74f8a87765cf2adaa31d6e
Anuj Agarwal <anujkrishna2002@gmail.com>
2024-09-22 00:18:30 +0530

    added skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
+import Skill from './components/Skill';
 const App =()=>{
   return (
     <>
@@ -10,6 +11,7 @@ const App =()=>{
     <main>
       <Hero/>
       <About/>
+      <Skill/>
     </main>
     </>
   )
