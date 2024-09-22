0d291e8b049b891cac08d936dcb0922e1169eacd
Anuj Agarwal <anujkrishna2002@gmail.com>
2024-09-22 15:11:06 +0530

    contact page added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skill from './components/Skill';
 import Work from './components/Work';
+import Contact from './components/Contact';
 const App =()=>{
   return (
     <>
@@ -14,6 +15,7 @@ const App =()=>{
       <About/>
       <Skill/>
       <Work/>
+      <Contact/>
     </main>
     </>
   )
